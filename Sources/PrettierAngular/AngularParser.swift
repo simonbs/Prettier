e3d125dc8a2493d9bbf2e24cf607ffbd0648307c
Simon Støvring <mail@simonbs.dk>
2022-05-03 17:18:44 +0200

    Fixes plugin name

--- Sources/PrettierAngular/AngularParser.swift
+++ Sources/PrettierAngular/AngularParser.swift
@@ -2,7 +2,7 @@ import Foundation
 import Prettier
 
 public struct AngularParser: Parser {
-    public let name = "parser-html"
+    public let name = "parser-angular"
     public let fileURL = Bundle.module.url(forResource: "parser-angular", withExtension: "js", subdirectory: "js")!
 
     public init() {}
